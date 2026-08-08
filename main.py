b6ba1db79a6217a61602a80e4fd5c3b809f56ba4
syota <q3sky0514@gmail.com>
2026-08-08 15:17:21 +0900

    名前記憶アプリの追加

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ print("hell world")
 
 st.title ("名前記憶アプリ")
 
-name=st.text_input("あなたの名前を入力してください")
-if st.button("名前を記録")：
+if "name"not in st.session_state:
+    st.session_state.name=""
 
-st.write(f"記憶している名前")
+name=st.text_input("あなたの名前を入力してください")
+if st.button("名前を記録"):
+    st.session_state.name=name
+
+st.write(f"記憶している名前:{st.session_state.name}")
