4a6939ce63f8df59759b616a83160953ffae8df1
syota <q3sky0514@gmail.com>
2026-08-08 15:23:16 +0900

    プリグラムのメモ

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
-print("hell world")
+# print("hell world")
 
-st.title ("名前記憶アプリ")
+# st.title ("名前記憶アプリ")
 
-if "name"not in st.session_state:
-    st.session_state.name=""
+# if "name"not in st.session_state:
+#     st.session_state.name=""
 
-name=st.text_input("あなたの名前を入力してください")
-if st.button("名前を記録"):
-    st.session_state.name=name
+# name=st.text_input("あなたの名前を入力してください")
+# if st.button("名前を記録"):
+#     st.session_state.name=name
 
-st.write(f"記憶している名前:{st.session_state.name}")
+# st.write(f"記憶している名前:{st.session_state.name}")
