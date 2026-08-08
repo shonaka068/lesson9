93307c53683004d8dce6739056f722535d35968a
syota <q3sky0514@gmail.com>
2026-08-08 16:02:10 +0900

    ユーザー情報入力の作成

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import streamlit as st
+
+st.title("ユーザー情報入力")
+
+if "user_name" not in st.session_state:
+    st.session_state.user_name=""
+
+name=st.text_input("あなたの名前を入力してください")
+if st.button("名前を保存"):
+    st.session_state.user_name=name
+    st.success(f"名前を保存しました")
+
+st.write(f"現在保存されている名前: {st.session_state.user_name}")
