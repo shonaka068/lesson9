5f3feef9ed5efc8f85d3d25a0d614d555b765527
syota <q3sky0514@gmail.com>
2026-08-08 17:27:20 +0900

    リセットの二重確認

--- pages/reset.py
+++ pages/reset.py
@@ -3,8 +3,9 @@ import streamlit as st
 st.title("ユーザー情報リセット")
 
 if st.button("情報をリセット"):
-    st.session_state.user_name = ""
-    st.session_state.age = ""
-    st.session_state.gakunenn = ""
-    st.session_state.syumi = ""
-    st.success("ユーザー情報がリセットされました")
+    if st.button("確認: 本当にリセットしますか？"):
+        st.session_state.user_name = ""
+        st.session_state.age = ""
+        st.session_state.gakunenn = ""
+        st.session_state.syumi = ""
+        st.success("ユーザー情報がリセットされました")
